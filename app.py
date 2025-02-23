a583f1fc182ad8b262ef23e0719997730ff0a540
Narawish <narawish_k@kkumail.com>
2025-02-23 18:18:40 +0700

    Add requirements.txt

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ from keras.models import load_model
 import streamlit as st
 from sklearn.preprocessing import MinMaxScaler
 
-yf.pdr_override()
-
-
+# yf.pdr_override()
+yf.set_tz_cache_location("custom/cache/location")
+# yf.enable_debug_mode()
 
 #Setting the title of webpage
 st.title('Stock Trend Prediction')
@@ -26,8 +26,12 @@ if start > end:
 if end <= dt.date.today() and start <= end:
     total_days = (end - start).days + 1
     st.write('total days = '  , total_days)
-    df = pdr.get_data_yahoo(user_input, start ,end)
+    # df = pdr.get_data_yahoo(user_input, start ,end)
+    # stock = yf.Ticker(user_input)
+    # df = stock.history(period="max")
+    df = yf.download(user_input, start=str(start) +" - 0500", end=str(end)+" - 0500")
 
+    print(df)
     #Arrange visualization into tabs
     tab1, tab2, tab3, tab4, tab5 = st.tabs(['Describe',
                                         'Closing',
@@ -73,56 +77,58 @@ if end <= dt.date.today() and start <= end:
         else:
             st.error('Insuffucient data')
     with tab5:
-        if total_days > 148:
-            #ML Part
-            # Splitting into training and testing data
+        try:
+            if total_days > 148:
+                #ML Part
+                # Splitting into training and testing data
 
-            data_training = pd.DataFrame(df['Close'][0:int(len(df)*0.70)])
-            data_testing = pd.DataFrame(df['Close'][int(len(df)*0.70):int(len(df))])
+                data_training = pd.DataFrame(df['Close'][0:int(len(df)*0.70)])
+                data_testing = pd.DataFrame(df['Close'][int(len(df)*0.70):int(len(df))])
 
-            #Scale the feature
+                #Scale the feature
 
-            scaler = MinMaxScaler(feature_range=(0,1))
-            data_training_array = scaler.fit_transform(data_training)
-
-
-            #Load model
-            model = load_model('keras_model.h5')
-
-            #Testing Part
-            final_df = pd.concat([data_training.tail(100),data_testing], axis=0)
-            input_data = scaler.fit_transform(final_df)
-
-            X_test = []
-            y_test = []
-
-            for i in range(100, input_data.shape[0]):
-                X_test.append(input_data[i-100:i])
-                y_test.append(input_data[i,0])
-
-            X_test, y_test = np.array(X_test), np.array(y_test)
+                scaler = MinMaxScaler(feature_range=(0,1))
+                data_training_array = scaler.fit_transform(data_training)
 
 
-            #Making predictions
-            y_predicted = model.predict(X_test)
+                #Load model
+                model = load_model('keras_model.h5')
 
-            scale_factor = 1/scaler.scale_[0]
-            y_predicted = y_predicted * scale_factor
-            y_test = y_test*scale_factor
+                #Testing Part
+                final_df = pd.concat([data_training.tail(100),data_testing], axis=0)
+                input_data = scaler.fit_transform(final_df)
+
+                X_test = []
+                y_test = []
+
+                for i in range(100, input_data.shape[0]):
+                    X_test.append(input_data[i-100:i])
+                    y_test.append(input_data[i,0])
+
+                X_test, y_test = np.array(X_test), np.array(y_test)
 
 
-            #LSTM Graph
-            st.subheader('Closing Price Prediction vs Original using LSTM')
-            fig = plt.figure(figsize = (12,6))
-            plt.plot(y_test,'b',label = 'Original Price')
-            plt.plot(y_predicted, 'r', label = 'Predicted Price')
-            plt.xlabel('Time (days)')
-            plt.ylabel('Price')
-            plt.legend()
-            st.pyplot(fig)
-        else:
-            st.error('Insuffucient data')
+                #Making predictions
+                y_predicted = model.predict(X_test)
 
+                scale_factor = 1/scaler.scale_[0]
+                y_predicted = y_predicted * scale_factor
+                y_test = y_test*scale_factor
+
+
+                #LSTM Graph
+                st.subheader('Closing Price Prediction vs Original using LSTM')
+                fig = plt.figure(figsize = (12,6))
+                plt.plot(y_test,'b',label = 'Original Price')
+                plt.plot(y_predicted, 'r', label = 'Predicted Price')
+                plt.xlabel('Time (days)')
+                plt.ylabel('Price')
+                plt.legend()
+                st.pyplot(fig)
+            else:
+                st.error('Insuffucient data')
+        except Exception as e:
+            st.error('Error fetching data')
 
 elif end > dt.date.today():
     st.write('Cannot see the future price')
